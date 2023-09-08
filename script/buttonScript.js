e26efd86b8112a6539225a7df65bc0a24fc716ed
5Cord <dima2v@bk.ru>
2023-09-09 01:18:40 +0300

    About edit

--- script/buttonScript.js
+++ script/buttonScript.js
@@ -1,3 +1,13 @@
   document.getElementById('backButton').addEventListener('click', function() {
     window.location.href = 'Index.html';
   });
+
+  const customCursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+    // Перемещаем курсор вместе с мышью
+    customCursor.style.left = `${e.clientX}px`;
+    customCursor.style.top = `${e.clientY}px`;
+});
+
+// Дополнительные эффекты можно добавить здесь, например, анимации при наведении на элементы

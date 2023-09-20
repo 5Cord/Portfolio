36f0883b2781ff2b28c52f20a8f9f852d64ea76b
5Cord <dima2v@bk.ru>
2023-09-20 17:49:18 +0300

    Adapt about

--- script/buttonScript.js
+++ script/buttonScript.js
@@ -12,3 +12,12 @@ document.addEventListener('mousemove', (e) => {
 
 // Дополнительные эффекты можно добавить здесь, например, анимации при наведении на элементы
 
+document.addEventListener("DOMContentLoaded", function() {
+  const modal = document.getElementById("myModal");
+
+  modal.classList.add("active"); // Добавляем класс "active" для активации модального окна
+
+  setTimeout(function() {
+      modal.classList.remove("active"); // Убираем класс "active" для исчезновения модального окна
+  }, 2000); // Закрыть модальное окно через 2 секунды
+});
